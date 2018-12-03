ff069d419a05b841ddf3d0aa50ab3a5944208d12
= <mikaiyl.davis@icloud.com>
2018-12-02 22:41:32 -0500

    Completed assessment

--- echo.py
+++ echo.py
@@ -5,18 +5,39 @@
 __author__ = "???"
 
 
-import sys
+import argparse
 
 
 def create_parser():
     """Creates and returns an argparse cmd line option parser"""
-    pass
+    parser = argparse.ArgumentParser(
+        description='Perform transformation on input text.')
+    # parser.add_argument('-h', '--help', help='prints this message')
+    parser.add_argument('-u', '--upper', action='store_true',
+                        help='convert text to uppercase', default=False)
+    parser.add_argument('-l', '--lower', action='store_true',
+                        help='convert text to lowercase', default=False)
+    parser.add_argument('-t', '--title', action='store_true',
+                        help='convert text to titlecase', default=False)
+    parser.add_argument('text', help='text to be manipulated')
+    return parser
 
 
 def main(args):
     """Implementation of echo"""
-    pass
+    result = args.text
+
+    if args.upper:
+        result = args.text.upper()
+
+    if args.lower:
+        result = args.text.lower()
+
+    if args.title:
+        result = args.text.title()
+
+    return result
 
 
 if __name__ == '__main__':
-    pass
+    print(main(create_parser().parse_args()))
--- tests/test_echo.py
+++ tests/test_echo.py
@@ -3,8 +3,95 @@
 
 import unittest
 import echo
+import subprocess
+
 
 # Your test case class goes here
+class TestEcho(unittest.TestCase):
+
+    def setUp(self):
+        self.parser = echo.create_parser()
+
+    def test_help(self):
+        """ Running the program without arguments should show usage. """
+
+        # Run the command `python ./echo.py -h` in a separate process, then
+        # collect it's output.
+        process = subprocess.Popen(
+            ["python", "echo.py", "-h"],
+            stdout=subprocess.PIPE)
+        stdout, _ = process.communicate()
+        usage = open("./USAGE", "r").read()
+
+        self.assertEquals(stdout, usage)
+
+    def test_u(self):
+        args = ['hello', '-u']
+        namespace = self.parser.parse_args(args)
+
+        self.assertTrue(namespace.upper)
+        self.assertEquals(echo.main(namespace), 'HELLO')
+
+    def test_upper(self):
+        args = ['hello', '--upper']
+        namespace = self.parser.parse_args(args)
+
+        self.assertTrue(namespace.upper)
+        self.assertEquals(echo.main(namespace), 'HELLO')
+
+    def test_l(self):
+        args = ['Hello', '-l']
+        namespace = self.parser.parse_args(args)
+
+        self.assertTrue(namespace.lower)
+        self.assertEquals(echo.main(namespace), 'hello')
+
+    def test_lower(self):
+        args = ['Hello', '--lower']
+        namespace = self.parser.parse_args(args)
+
+        self.assertTrue(namespace.lower)
+        self.assertEquals(echo.main(namespace), 'hello')
+
+    def test_t(self):
+        args = ['hello', '-t']
+        namespace = self.parser.parse_args(args)
+
+        self.assertTrue(namespace.title)
+        self.assertEquals(echo.main(namespace), 'Hello')
+
+    def test_title(self):
+        args = ['hello', '--title']
+        namespace = self.parser.parse_args(args)
+
+        self.assertTrue(namespace.title)
+        self.assertEquals(echo.main(namespace), 'Hello')
+
+    # def test_all(self):
+    #     process = subprocess.Popen(
+    #         ["python", "echo.py", "-tul", "test"],
+    #         stdout=subprocess.PIPE)
+    #     stdout, _ = process.communicate()
+    #     self.assertTrue(stdout.islower())
+
+    #     process = subprocess.Popen(
+    #         ["python", "echo.py", "-tlu", "test"],
+    #         stdout=subprocess.PIPE)
+    #     stdout, _ = process.communicate()
+    #     self.assertTrue(stdout.isupper())
+
+    #     process = subprocess.Popen(
+    #         ["python", "echo.py", "-ult", "test"],
+    #         stdout=subprocess.PIPE)
+    #     stdout, _ = process.communicate()
+    #     self.assertTrue(stdout.istitle())
+
+    def test_none(self):
+        args = ['Hello']
+        namespace = self.parser.parse_args(args)
+
+        self.assertTrue(namespace.text)
+        self.assertEquals(echo.main(namespace), 'Hello')
 
 
 if __name__ == '__main__':
